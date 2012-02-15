577972b88968f55a7e4d4e0f73165471fbcd2d08
007slm <007slm@3a9d6a52-fd0c-2442-b21a-e0fa8ddcac03>
2012-02-15 01:17:11 +0000

    d盘代码整理归档

---
+++ trunk/slmFrameWork/test/event/Door.java
@@ -0,0 +1,21 @@
+package event;
+
+public class Door {
+	private String state;
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+	
+	
+}
---
+++ trunk/slmFrameWork/test/event/DoorEventListener.java
@@ -0,0 +1,13 @@
+package event;
+
+import java.util.EventListener;
+
+public class DoorEventListener implements EventListener {
+	//每个不同的方法监听不同的事件 然后做监听后的处理逻辑
+	void doorCloseEvent() {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+	
+
+}
